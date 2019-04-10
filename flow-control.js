412dac070d18824dd33b50b78087054dda4fc9bc
Karen Simmering <karen.simmering@gmail.com>
2019-04-10 02:44:10 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -13,7 +13,13 @@ return "You are not a teenager"
 }
 
 function ageChecker(age) {
-
+if (age >= 13 && age <= 19){
+  return "You are a teenager!"
+} elseif (age <= 12){
+  return "You are a kid"
+} elseif (age > 19){
+  return "You are a grownup"
+}
 }
 
 function ternaryTeenager(age) {
